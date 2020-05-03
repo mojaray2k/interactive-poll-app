3987b020e1e634eb552376c3e16db1faeb64ee7f
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-03 11:28:22 -0400

    added loading bar

--- src/actions/share.js
+++ src/actions/share.js
@@ -2,15 +2,18 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receivePolls } from "./polls";
 import { setAuthUser } from "./authUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const AUTH_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, polls }) => {
       dispatch(receiveUsers(users));
       dispatch(receivePolls(polls));
       dispatch(setAuthUser(AUTH_ID));
+      dispatch(hideLoading());
     });
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authUser from "./authUser";
 import users from "./users";
 import polls from "./polls";
+import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   authUser,
   users,
   polls,
+  loadingBar: loadingBarReducer,
 });
