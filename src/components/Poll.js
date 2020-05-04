fa321ce7ae3cf961b864fd3eb3c2c5bcc77b46ba
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-04 12:59:31 -0400

    implimented adding poll answers to state

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getPercentage } from "../utils/helpers";
+import { handleAddAnswer } from "../actions/answers";
 
 const getVoteKeys = () => ["aVotes", "bVotes", "cVotes", "dVotes"];
 
@@ -8,7 +9,14 @@ class Poll extends Component {
   handleAnswer = (answer) => {
     const { poll, authedUser } = this.props;
     this.answered = true;
-    console.log("Add Answer: ", answer);
+    // console.log("Add Answer: ", answer);
+    this.props.dispatch(
+      handleAddAnswer({
+        authedUser,
+        answer,
+        id: poll.id,
+      })
+    );
   };
 
   render() {
@@ -40,7 +48,7 @@ class Poll extends Component {
                     this.handleAnswer(key[0]);
                   }
                 }}
-                key={poll[key]}
+                key={key}
                 className={`option ${vote === key[0] ? "chosen" : ""}`}
               >
                 {vote === null ? (
